98ced52788e8dd1c090f6a67c06782fad4a20772
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-19 19:27:12 +0000

    Removed presence: true validation on password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :role, presence: true
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :phone_number, presence: true, :if => :branch?
-  validates :password, presence: true, length: { minimum: 8 }, on: :create
+  validates :password, length: { minimum: 8 }, on: :create
 
   def branch?
     role == 'branch'
