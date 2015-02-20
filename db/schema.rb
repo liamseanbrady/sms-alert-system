86ab132192a1de32426ca1206d2f9ad57495dcba
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-20 15:26:46 +0000

    Add tasks table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219135754) do
+ActiveRecord::Schema.define(version: 20150220151517) do
+
+  create_table "phrases", force: true do |t|
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
