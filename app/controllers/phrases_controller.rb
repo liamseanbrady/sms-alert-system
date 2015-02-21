9685a765d1469d934f0acd7b54a8e33e91d91eda
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-21 23:34:10 +0000

    Add create action.

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,5 +1,19 @@
 class PhrasesController < ApplicationController
+  before_action :require_user
+
   def new
     @phrase = Phrase.new
+  end
+
+  def create
+    @phrase = Phrase.new(phrase_params)
+
+    if @phrase.save
+      flash[:notice] = 'You created a new phrase'
+      redirect_to user_path(current_user)
+    else
+      flash[:error] = 'The phrase could not be created. Please try again.'
+      render :new
+    end
   end
 end
