8d0ec4d9e18c4dd26c4ed8ec6a06cb339a824a0f
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-22 22:24:26 +0000

    Add new and create actions.

---
+++ app/controllers/alerts_controller.rb
@@ -0,0 +1,24 @@
+class AlertsController < ApplicationController
+  before_action :require_user
+  
+  def index
+    @alerts = Alert.all.order('created_at desc')
+  end
+
+  def new
+    @alert = Alert.new
+    @phrases = Phrase.all        
+  end
+
+  def create
+    phrase = Phrase.find(params[:phrase_id]).body
+    @alert = Alert.new(phrase: phrase)
+
+    if @alert.save
+      flash[:notice] = 'The alert was sent successfully'
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+end
