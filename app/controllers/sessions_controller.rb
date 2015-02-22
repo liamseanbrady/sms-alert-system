3dac75346d70d3584baafabc42d547c5a11c0d22
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-22 22:35:31 +0000

    Use flash.now instead of flash for unhappy path in create action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash[:error] = "There's something wrong with your username or password. Please try again"
+      flash.now[:error] = "There's something wrong with your username or password. Please try again"
       render :new
     end
   end
