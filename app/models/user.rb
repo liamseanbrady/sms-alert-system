34274791f9e8a5ee6d1d012613b5252f2b1a0a4d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-22 22:21:15 +0000

    Add branches scope.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
  
   has_many :tasks
+  
+  scope :branches, -> { where role: 'branch' }
 
   validates :role, presence: true
   validates :username, presence: true, uniqueness: true
