0548a1e45aa3e7112b0b147e71c6b08ca61f297f
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-22 17:19:14 +0000

    Add new and create actions.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < AppliationController
+  before_action :require_user
+
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = 'Your task was successfully created'
+      redirect_to user_path(current_user)
+    else
+      flash[:error] = 'The task could not be created'
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).require(:phone_number, :phrase, :recipient)
   end
 end
