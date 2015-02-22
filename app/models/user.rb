b7238b34ffe2c092c961eaa01f8505a3255dd1d1
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-22 17:20:25 +0000

    Add has_many association for tasks.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
+ 
+  has_many :tasks
 
   validates :role, presence: true
   validates :username, presence: true, uniqueness: true
