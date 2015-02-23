f50839ad005a038b2888cfa78ea6d0b6af71b87a
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-23 00:21:43 +0000

    Use Rails.configuration to access cofiguration variables.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -5,13 +5,13 @@ class Alert < ActiveRecord::Base
     User.branches.each do |branch|
       task = Task.create(phone_number: branch.phone_number, phrase: self.phrase, recipient: branch)
       # send message using twilio
-      account_sid = ENV['TWILIO_SID'] 
-      auth_token = ENV['TWILIO_TOKEN']
+      account_sid = Rails.configuration.twilio_sid
+      auth_token = Rails.configuration.twilio_token
       @client = Twilio::REST::Client.new account_sid, auth_token
 
       message = @client.account.messages.create(:body => "#{self.phrase}",
           :to => "#{branch.phone_number}",
-          :from => ENV['TWILIO_PHONE_NUMBER'])
+          :from => Rails.configuration.twilio_phone_number)
     end
   end
 end
