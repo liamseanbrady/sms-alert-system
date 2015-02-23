58951500846347cf95353acf3bd385481a1799a1
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-23 19:29:53 +0000

    Add to_csv class method.

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,13 @@ class Task < ActiveRecord::Base
   validates :phone_number, presence: true
   validates :phrase, presence: true
   validates :recipient, presence: true
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |task|
+        csv << task.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
