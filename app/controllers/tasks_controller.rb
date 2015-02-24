fa5604e66ee17900e5f413cf27f982c00353b6d1
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-24 23:45:58 +0000

    Add completed action.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :require_admin
+  before_action :require_admin, only: [:index]
   
   def index
     @tasks = Task.all
@@ -7,4 +7,17 @@ class TasksController < ApplicationController
       format.csv { render text: Task.to_csv } 
     end
   end
+
+  def completed
+    @task = Task.find(params[:id])
+
+    if @task.update(completed_at: Time.now)
+      flash[:notice] = "The task was marked as completed"
+    else
+      flash[:error] = "Task could not be marked as completed. Please try again."
+    end
+
+    redirect_to user_path(current_user)
+  end
+  
 end
