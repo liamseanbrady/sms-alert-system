285bb093cf0693d51ede6267317bcb1f6101c436
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-24 23:48:40 +0000

    Improve csv output.

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,9 +7,9 @@ class Task < ActiveRecord::Base
 
   def self.to_csv
     CSV.generate do |csv|
-      csv << column_names
+      csv << (column_names + ['branch_name'])
       all.each do |task|
-        csv << task.attributes.values_at(*column_names)
+        csv << (task.attributes.values_at(*column_names) + [task.recipient.username]) 
       end
     end
   end
