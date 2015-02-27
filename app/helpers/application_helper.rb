273589c4b33d8d2a36e5bd8fdac2b686fa517c3d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-27 02:45:07 +0000

    Change format of datetime in friendly_datetime. Add generate_filename method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
   def friendly_datetime(dt)
-    dt = dt.strftime("%a %e %b %Y") 
+    dt = dt.strftime("%a %e %b %Y  (%H:%M %P)") 
+  end
+
+  def generate_filename
+    clean_time = Time.now.strftime("%e_%b_%y")
+    optional_prefix = current_user.branch? ? "#{current_user.username.gsub(/[^a-zA-Z0-9]/, '_').downcase}" : 'all_sites'
+    "#{optional_prefix}_fire_safety_logs_#{clean_time}"
   end
 end
